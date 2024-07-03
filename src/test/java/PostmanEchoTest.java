323920b0c685824af308e4e73e508caa9d281091
Olga Khamitova <olgacherkasova13317@rambler.ru>
2024-07-03 11:59:05 +0300

    gradle.yml is added

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PostmanEchoTest {
+    @Test
+    public void test_PostmanEcho() {
+          // Given - When - Then
+         // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+         // Выполняемые действия
+                .when()
+                .post("/post")
+         // Проверки
+                .then()
+                .statusCode(200)
+                .body(/* --> ваша проверка здесь <-- */)
+        ;
+    }
+}
+
+
+
+
+
